c5cde4391120d2b1933ccbdcfa1beff36463aad2
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-26 17:08:26 -0400

    back

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const MapScreen = () => {
+    const navigation= useNavigation()
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView>
+        <TouchableOpacity onPress={() => {navigation.goBack()}}>
+            <Text>MapScreen</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
