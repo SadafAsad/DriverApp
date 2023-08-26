6484a70c354a0b66f8ba9d3aac5eb072a104bfd1
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-26 18:28:31 -0400

    pushing data on store

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,38 +3,53 @@ import React from 'react'
 import NavOptions from '../components/NavOptions'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from "@env"
+import { useDispatch } from 'react-redux'
+import { setOrigin, setDestination } from '../slices/navSlice'
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView className='bg-white h-full'>
-        <View className='p-5'>
-            <Image 
-                source={{uri: 'https://links.papareact.com/gzs'}}
-                style={{resizeMode: 'contain', width: 100, height: 100}}
-            />
+    const dispatch = useDispatch()
 
-            <GooglePlacesAutocomplete 
-                placeholder='Where From?'
-                styles={{
-                    container: {
-                        flex: 0,
-                    },
-                    textInput: {
-                        fontSize: 18,
-                    }
-                }}
-                query={{
-                    key: GOOGLE_MAPS_APIKEY,
-                    language: 'en'
-                }}
-                nearbyPlacesAPI='GooglePlacesSearch'
-                debounce={400}
-            />
+    return (
+        <SafeAreaView className='bg-white h-full'>
+            <View className='p-5'>
+                <Image 
+                    source={{uri: 'https://links.papareact.com/gzs'}}
+                    style={{resizeMode: 'contain', width: 100, height: 100}}
+                />
 
-            <NavOptions />
-        </View>
-    </SafeAreaView>
-  )
+                <GooglePlacesAutocomplete 
+                    placeholder='Where From?'
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        }
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+                        dispatch(setDestination(null))
+                    }}  
+                    fetchDetails={true}
+                    returnKeyType={'search'}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en'
+                    }}
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                />
+
+                <NavOptions />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
