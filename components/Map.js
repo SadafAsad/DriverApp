32847030ae7bce82a1a05bbf130f23d78ac97b27
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-26 18:39:08 -0400

    Map modified

--- components/Map.js
+++ components/Map.js
@@ -1,20 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import MapView, { Marker } from 'react-native-maps'
+import { useSelector } from 'react-redux'
+import { selectOrigin } from '../slices/navSlice'
 
 const Map = () => {
-  return (
-    <MapView 
-        className='flex-1'
-        mapType='mutedStandard'
-        initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-        }}
-    />
-  )
+    const origin = useSelector(selectOrigin)
+
+    return (
+        <MapView 
+            className='flex-1'
+            mapType='mutedStandard'
+            initialRegion={{
+                latitude: origin.location.lat,
+                longitude: origin.location.lng,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005
+            }}
+        />
+    )
 }
 
 export default Map
