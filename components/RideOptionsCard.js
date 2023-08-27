a15eb1337283c3d9addd461802191fdb17a83d9d
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-27 14:58:10 -0400

    ui bug fix

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Icon } from '@rneui/base'
+import { useNavigation } from '@react-navigation/native'
 
 const RideOptionsCard = () => {
-  return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
-  )
+    const navigation = useNavigation()
+    
+    return (
+        <SafeAreaView className='bg-white flex-grow'>
+            <View>
+                <TouchableOpacity 
+                    className='absolute top-3 left-5 p-3 rounded-full z-50'
+                    onPress={() => navigation.navigate('NavigateCard')}
+                >
+                    <Icon name='chevron-left' type='fontawesome'/>
+                </TouchableOpacity>
+                <Text className='text-center py-5 text-xl'>Select a Ride</Text>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default RideOptionsCard
