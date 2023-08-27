e4e1978219151ed4e395225a1d6643b3ea64f8d3
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-27 08:48:26 -0400

    Map screen second half

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,25 +1,33 @@
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Map from '../components/Map'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreen = () => {
     const navigation= useNavigation()
-  return (
-    <View>
-        <TouchableOpacity onPress={() => {navigation.goBack()}}>
-            <Text>MapScreen</Text>
-        </TouchableOpacity>
+    const Stack = createNativeStackNavigator()
 
-        <View className='h-1/2'>
-            <Map />
+    return (
+        <View>
+            <TouchableOpacity onPress={() => {navigation.goBack()}}>
+                <Text>MapScreen</Text>
+            </TouchableOpacity>
+
+            <View className='h-1/2'>
+                <Map />
+            </View>
+
+            <View className='h-1/2'>
+                <Stack.Navigator>
+                    <Stack.Screen name='NavigateCard' component={NavigateCard} options={{headerShown: false}}/>
+                    <Stack.Screen name='RideOptions' component={RideOptionsCard} options={{headerShown: false}}/>
+                </Stack.Navigator>
+            </View>
         </View>
-
-        <View className='h-1/2'>
-
-        </View>
-    </View>
-  )
+    )
 }
 
 export default MapScreen
