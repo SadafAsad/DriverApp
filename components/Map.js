2f7a784281a1120517ce7b3e9d82371144584d05
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-27 15:51:59 -0400

    info calculations

--- components/Map.js
+++ components/Map.js
@@ -13,11 +13,23 @@ const Map = () => {
 
     // zoom out to fit markers
     useEffect(() => {
-        if(!origin || !destination) return
+        if (!origin || !destination) return
         mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
             edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
         })
     }, [origin, destination])
+
+    useEffect(() => {
+        if (!origin || !destination) return
+        const getTravelTime = async () => {
+            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`)
+            .then((res) => res.json())
+            .then(data => {
+            })
+            
+        }
+        getTravelTime()
+    }, [origin, destination, GOOGLE_MAPS_APIKEY])
 
     return (
         <MapView 
