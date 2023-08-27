c7972021c47d2a53b61eacf1fd959994ffb545c5
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-27 16:14:27 -0400

    Final touch

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -5,6 +5,7 @@ import Map from '../components/Map'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from '@rneui/base';
 
 const MapScreen = () => {
     const navigation= useNavigation()
@@ -12,6 +13,13 @@ const MapScreen = () => {
 
     return (
         <View>
+            <TouchableOpacity 
+                onPress={() => {navigation.navigate('Home')}}
+                className='absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg'
+            >
+                <Icon name='menu'/>
+            </TouchableOpacity>
+
             <View className='h-1/2'>
                 <Map />
             </View>
