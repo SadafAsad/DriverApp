a94f2b30b7922db4cb1ff7096b1282dfcba02d9a
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-08-27 14:29:31 -0400

    flatlist seperator

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -22,6 +22,12 @@ const NavFavourites = () => {
         <FlatList
             data={data}
             keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => (
+                <View
+                    className='bg-gray-200'
+                    style={{height: 0.5}}
+                />
+            )}
             renderItem={({item}) => (
                 <TouchableOpacity className='flex-row items-center p-5'>
                     <View className='mr-4 rounded-full bg-gray-300 p-3'>
